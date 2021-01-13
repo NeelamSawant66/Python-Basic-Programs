eb1ea138c26ebeab1259e6362021b9d98a192383
Neelam Sawant <65168950+NeelamSawant66@users.noreply.github.com>
2021-01-13 11:06:51 +0530

    Update Weight Calculator.py

--- Weight Calculator.py
+++ Weight Calculator.py
@@ -1,10 +1,11 @@
 #done
 
 print(''' Welcome To Weight Calculator
-Select Any One Option
+
  1. Pounds To Kilogram
  2. Kilogram To Pounds''')
-num = int(input('You have selected: '))
+def weight():
+num = int(input('Select Any One Option: '))
 if num ==1:
     weight1= float(input("Enter Weight in Pounds: "))
     result1 = weight1 * 0.45
@@ -13,3 +14,7 @@ elif num ==2:
     weight2= float(input("Enter Weight in Kilograms: "))
     result2 = weight2 / 0.45
     print(f"{weight2}kg = {result2}lbs")
+        else:
+        print("Invaild Input, Try Again !!!")
+        weight()
+weight()
